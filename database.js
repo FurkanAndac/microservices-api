02d7bfd5e4d3e9e76f59469182a063f528b74c39
Furkan Andac <FurkanAndac>
2024-01-25 18:53:45 +0100

    add fixie socks for proxy outbound connections

--- database.js
+++ database.js
@@ -1,11 +1,17 @@
 // database.js
 require('dotenv').config(); // Load environment variables from .env file
 const mongoose = require('mongoose');
+const fixieData = process.env.FIXIE_SOCKS_HOST.split(new RegExp('[/(:\\/@/]+'));
+
 
 function connectToDatabase() {
   mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    proxyUsername: fixieData[0],
+    proxyPassword: fixieData[1],
+    proxyHost: fixieData[2],
+    proxyPort: fixieData[3]
   });
 
   const db = mongoose.connection;
